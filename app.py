47705d74dcb195c7bbc81ee0a16d5267ea8f0be0
Sujith004 <sujithbaskaran18@gamil.com>
2025-08-27 20:21:47 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model=joblib.load("student_pass_model.pkl")
+st.title("Student pass prediction")
+st.write("Enter the scores below to predict the student will pass or fail")
+
+
+math=st.number_input("Math score",min_value=0,max_value=100,step=1)
+reading=st.number_input("Reading score",min_value=0,max_value=100,step=1)
+writing=st.number_input("Writing score",min_value=0,max_value=100,step=1)
+
+if st.button("Predict"):
+    input_data=pd.DataFrame([[math,reading,writing]],
+                            columns=['math score','reading score','writing score'])
+    prediction=model.predict(input_data)[0]
+    probability=model.predict_proba(input_data)[0]
+    if prediction==1:
+       label='pass'
+       confidence=probability[1]
+    else:
+        label='fail'
+        confidence=probability[0]
+    st.write(f"The student is predicted to {label} with {confidence*100:.2f}% confidence")
